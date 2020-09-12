763750687df7e9da351e694eafb276212cac4396
Roger <roger990131@gmail.com>
2020-09-12 16:39:14 +0800

    commit

--- questions/1~20/005.Longest Palindromic Substring/main.cpp
+++ questions/1~20/005.Longest Palindromic Substring/main.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         string res;
-        
-        for (int i = 0; i < s.size(); i ++)
-        {
-            for (int j = i, k = i; j >= 0 && k < s.size() && s[j] == s[k]; j --, k ++)
-            {
-                if (res.size() < k - j + 1)
-                    res = s.substr(j, k - j + 1);
-            }
 
-            for (int j = i, k = i + 1; j >= 0 && k < s.size() && s[j] == s[k]; j --, k ++)
-            {
-                if (res.size() < k - j + 1)
-                    res = s.substr(j, k - j + 1);
-            }
+        for (int i = 0; i < s.size(); i ++) {
+            int l = i - 1, r = i + 1;
+            while (l >= 0 && r < s.size() && s[l] == s[r]) l --, r ++;
+            if (r - l - 1 > res.size()) res = s.substr(l + 1, r - l - 1);
+
+            l = i, r = i + 1;
+            while (l >= 0 && r < s.size() && s[l] == s[r]) l --, r ++;
+            if (r - l - 1 > res.size()) res = s.substr(l + 1, r - l - 1);
         }
 
         return res;
